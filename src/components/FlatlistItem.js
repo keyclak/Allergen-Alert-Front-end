0fd5f88fbef87f992885683e9faa28df078e3b22
catalinasandoval <asandoval4@wisc.edu>
2020-10-26 21:31:50 -0500

    Added working navigation to Settings page

--- src/components/FlatlistItem.js
+++ src/components/FlatlistItem.js
@@ -1,16 +1,15 @@
 import React, {useState} from 'react'
+import { useNavigation } from '@react-navigation/native'
 import { View, Text, StyleSheet, Pressable, Image} from 'react-native';
 import { ListItem, Icon } from 'react-native-elements'
 import { StyleConstants, Styles, Colors } from '../style';
 
 export default function FlatlistItem(props) {
 
-    function handlePress(location) {
-        console.log(location)
-    }
-
+    const navigation = useNavigation(); 
+    
     return(
-        <ListItem key={props.i} bottomDivider onPress={() => {handlePress(props.item.navTo)}}>
+        <ListItem key={props.i} bottomDivider onPress={() => { navigation.navigate(props.item.navTo) }}>
             <Icon name={props.item.icon} color={"#666"}/>
             <ListItem.Content>
             <ListItem.Title style={[Styles.labelText, {color:"#666"}]}>{props.item.title}</ListItem.Title>
