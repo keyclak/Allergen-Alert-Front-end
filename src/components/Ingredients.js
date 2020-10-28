ad6dbec2156fa859beb2bbbf359bdf99cf25911e
Allison Rokus <rokus@wisc.edu>
2020-10-28 16:43:15 -0500

    Format flatlist

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,17 +1,22 @@
 import React from 'react'
 import { View, Text, StyleSheet, Pressable, Image} from 'react-native';
 import { FlatList } from 'react-native-gesture-handler'
+import { color } from 'react-native-reanimated';
 
 export default function Ingredients(props) {
     return (
-        <View style = {{width: 100, height: 100}}>
-            <Text>Ingredients</Text>
+        <View style = {[{width: 250}, {height: 150},{marginTop:25} ]}>
+            <Text style={[{ color: "white" }]}>
+                Ingredients
+            </Text>
             <FlatList
-                style = {{borderWidth: 2}}
+                style = {[{borderWidth: 2}, {marginTop:5}]}
                 data = {props.content}
                 renderItem={({item}) => (
                     <View>
-                        <Text>{item.name}</Text>
+                        <Text style={[{ color: "white" }, {paddingLeft: 10}, {fontSize: 14}]}> 
+                            {item.name}
+                        </Text>
                     </View>
                 )}
             />
