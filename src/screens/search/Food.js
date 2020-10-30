97b3c8fedce7be3c8224ae91d44106516448d327
catalinasandoval <asandoval4@wisc.edu>
2020-10-30 13:03:31 -0500

    Made this page scrollable and ingredients listed directly to screen

--- src/screens/search/Food.js
+++ src/screens/search/Food.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import { View, Text, StyleSheet, Pressable, Image, TouchableOpacity, Alert} from 'react-native';
+import { View, ScrollView, Text, SafeAreaView, StyleSheet, Pressable, Image, TouchableOpacity, Alert} from 'react-native';
 import { StyleConstants, Styles, Colors } from '../../style';
 import Ingredients from '../../components/Ingredients'
 import { color } from 'react-native-reanimated';
@@ -7,8 +7,6 @@ import { useDummy, useGetUpcSearch } from '../../hooks/api';
 
 export default function Food({ navigation, route }) {
     const upc = route.params.upc;
-    console.log(upc);
-
     const getUpcSearch = useGetUpcSearch();
 
     useEffect(() => {
@@ -26,39 +24,38 @@ export default function Food({ navigation, route }) {
 
     useEffect(() => console.log(getUpcSearch.response), [getUpcSearch.response]);
 
+    const listIngredients = (ingredients) => {
+        var x;
+        var text = ""; 
+
+        for(x in ingredients) {
+            text += "- " + ingredients[x].name + "\n"
+        }
+
+        return(
+            <Text style={[Styles.ingredientList]}>{text}</Text>
+        )
+    }
+
     const disclaimer = "Basic legal disclaimer"
 
     return(
-        <View style={[Styles.container, {justifyContent:'space-evenly'}]}>  
-            <View style={{display:'flex', direction:'column', alignItems: 'center'}}>
-                <Text style={[Styles.titleText, {fontSize: 45}]}>{getUpcSearch.response?.name}</Text> 
+        <SafeAreaView style={[Styles.container, {justifyContent:'space-evenly'}]}>  
+            <ScrollView style={{display: 'flex', flexDirection: 'column', width: StyleConstants.FormWidth}}> 
+                <View style={{display:'flex', direction:'column', alignItems: 'center'}}>
+                    <Text style={[Styles.titleText, {marginTop: 100, fontSize: 45}]}>{getUpcSearch.response?.name}</Text> 
+                </View>
                 <View style={[Styles.alertBox, (getUpcSearch.response?.safe ? null : Styles.alert)]}>
                     <Text style={Styles.subtitleText}>{(getUpcSearch.response?.safe ? 'This food is safe!' : 'This food is not safe!')}</Text>
                 </View>
-            </View>
-            <View style={{display: 'flex', alignItems: 'center'}}>
-                <Ingredients content={getUpcSearch.response?.ingredients} />
-            </View>
-            {/* <View style={[{flexDirection: "row"}]}>
-                <TouchableOpacity
-                    style={Styles.navButton}
-                    onPress= {null}
-                    underlayColor='#fff'>
-                    <Text style = {Styles.navButtonText} > Back</Text>
-                </TouchableOpacity>
-
-                <View style={Styles.buttonSpace} />
-
-                <TouchableOpacity
-                    style={Styles.navButton}
-                    onPress= {null}
-                    underlayColor='#fff'>
-                    <Text style = {Styles.navButtonText} >Search Another Item</Text>
-                </TouchableOpacity>
-            </View> */}
-            <View style={{alignSelf: 'auto'}}>
-                <Text style={[Styles.labelText, {marginLeft: 0}]}>{disclaimer}</Text>
-            </View>
-        </View>
+                <View>
+                    <Text style={[{ paddingTop: 50, color: Colors.Foreground, fontSize: 20, textAlign: 'center', textDecorationLine: 'underline'}]}>Ingredients</Text>
+                    {listIngredients(getUpcSearch.response?.ingredients)}
+                </View>
+                <View style={{alignSelf: 'center', paddingTop: StyleConstants.Radius}}>
+                    <Text style={[Styles.labelText, {color: Colors.Foreground, marginLeft: 0}]}>{disclaimer}</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
