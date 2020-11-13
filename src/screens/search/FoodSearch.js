de36dcdd14a71e955c88ed87ea50edb25157569e
Allison Rokus <rokus@wisc.edu>
2020-11-13 01:04:04 -0600

    added search button

--- src/screens/search/FoodSearch.js
+++ src/screens/search/FoodSearch.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { View, StatusBar, StyleSheet, Text, TouchableOpacity, Image, TextInput, SafeAreaView, FlatList } from "react-native";
+import { View, StatusBar, StyleSheet, Text, TouchableOpacity, Button, TextInput, SafeAreaView, FlatList } from "react-native";
 import { StyleConstants, Styles, Colors } from '../../style';
 import { useGetFoodSearch } from '../../hooks/api';
 import { useDummy } from '../../hooks/api';
@@ -9,32 +9,20 @@ function searchHelper(query) {
 }
 
 export default function FoodSearch({navigation}) {
-    //const getFoodSearch = useDummy();
 
     const [searchValue, setSearchValue] = useState("");
-    const [results, setResults] = useState([]);
     const getFoodSearch = useGetFoodSearch(searchValue);
+  //  const [query, setQuery] = useState('');
 
-    const fetchData = async (t) => {
-        //console.log(t)
-        setSearchValue(t);
-        getFoodSearch.background(); //
-        setResults(getFoodSearch.response)
-        //console.log(results)
+    const fetchData = async () => {
+   //     console.log(query);        
+        getFoodSearch.background(); 
       };
-
-    useEffect(() => {
-    fetchData();
-    }, []);
-
-    useEffect(() => {
-        console.log(getFoodSearch.response)
-    }, [getFoodSearch.response]);
 
 
     //creates an item object from data for flatlist
     const Item = ({ item }) => (
-        <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('FoodPage', { upc: item.id })}>
+        <TouchableOpacity style={styles.item} onPress={() => navigation.navigate('FoodPage', { foodId: item.id })}>
             <Text style={{fontSize: 20}}>{item.name}</Text>
         </TouchableOpacity>
     );
@@ -42,17 +30,28 @@ export default function FoodSearch({navigation}) {
     return (
         <View style={Styles.container}>
             <View style={{justifyContent: 'center', alignItems: 'center', flexDirection: 'row'}}>
-                <View style={{width: '100%', borderRadius: 20}}>
-                    <TextInput style={styles.inputFood}
+                <View style={{ borderRadius: 20, alignSelf: 'center', flexDirection: 'row', justifyContent:'space-evenly' }}>
+                    <TextInput style={styles.inputFood }
                         placeholder="  Search Food"
-                        onChangeText={t => fetchData(t)}
+                        onChangeText= {t => setSearchValue(t)}
+                        onSubmitEditing={() => fetchData()}
                     />
+
+                    <View style={{backgroundColor: Colors.ButtonBackground, borderRadius: 20, alignContent: 'center',justifyContent: 'center'}}> 
+                        <Button 
+                            button backgroundColor
+                            onPress={() => fetchData()}
+                            title="Search"
+                            color='#FFF'                        
+                        />
+                    </View>
                 </View>
             </View>
             <FlatList
-                data={results}
+                data={getFoodSearch.response}
                 keyExtractor={item => item.id}
-                renderItem={Item} />
+                renderItem={Item} 
+                extraData={getFoodSearch.response}/>
         </View>
     );
 }
@@ -60,16 +59,16 @@ export default function FoodSearch({navigation}) {
 const styles = StyleSheet.create({
     inputFood: {
         alignItems: 'center',
+        width: '75%',
         alignSelf: 'center',
         borderRadius: 20,
-        width: '90%',
-        borderColor: '#FC9F24',
+        borderColor: Colors.ButtonBackground,
         borderWidth: 5,
         fontSize: 25,
         height: 50,
     },
     item: {
-        backgroundColor: '#FC9F24',
+        backgroundColor: Colors.ButtonBackground,
         padding: 15,
         marginVertical: 5,
         width: '95%',
