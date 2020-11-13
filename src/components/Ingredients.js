63bcd9145ee6736941e7a26429fd9d7ac547de92
catalinasandoval <asandoval4@wisc.edu>
2020-11-12 21:24:15 -0600

    Rough ingredient display

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import { View } from 'react-native';
+import { View, FlatList, Text } from 'react-native';
 import { ListItem } from 'react-native-elements'
 import { Styles, Colors } from '../style';
 
 export default function Ingredients(props) {
     return (
         <View>
+            {/* {console.log("food: " + props.content[0].ingredient)}
         {
             (props.content).map((item, i) => (
             <ListItem key={i} containerStyle={{backgroundColor: Colors.Background, padding: 2}}>
@@ -14,7 +15,20 @@ export default function Ingredients(props) {
                 </ListItem.Content>
             </ListItem>
             ))
-        }
+        } */}
+        
+        <FlatList
+                style = {[{marginTop:7.5}]}
+                data = {props.content}
+                renderItem={({item}) => (
+                    <View>
+                        <Text style={item.safe ? [Styles.ingredientList] : [Styles.ingredientList, {color: Colors.Error}]}> 
+                            {item.ingredient}
+                        </Text>
+                    </View>
+                )}
+                keyExtractor={item => `${item.ingredient}`}
+            />
         </View>
     )
 }
--- src/screens/search/Food.js
+++ src/screens/search/Food.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import { View, ScrollView, Text, SafeAreaView, StyleSheet, Pressable, Image, TouchableOpacity, Alert} from 'react-native';
 import { StyleConstants, Styles, Colors } from '../../style';
+import { ListItem } from 'react-native-elements'
 import Ingredients from '../../components/Ingredients'
 import { color } from 'react-native-reanimated';
 import { useDummy, useGetUpcSearch, useAddToGroceryList } from '../../hooks/api';
@@ -31,6 +32,19 @@ export default function Food({ navigation, route }) {
         .catch(e => {});
     }
 
+    const listIngredients = (ingredients) => {
+        // console.log("food: " + ingredients[0].ingredient)
+        return (
+            (ingredients).map((item, i) => (
+            <ListItem key={i} containerStyle={{backgroundColor: Colors.Background, padding: 2}}>
+                <ListItem.Content>
+                    <ListItem.Title style={item.safe ? [Styles.ingredientList] : [Styles.ingredientList, {color: Colors.Error}]}>{item.ingredient}</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+            ))
+        )
+    }
+
     const disclaimer = "Basic Legal Disclaimer"
 
     return(
@@ -52,6 +66,10 @@ export default function Food({ navigation, route }) {
                 </View>
                 <Text style={[{ paddingTop: 20, color: Colors.Foreground, fontSize: 20, textAlign: 'center', textDecorationLine: 'underline'}]}>Ingredients</Text>
                 <Ingredients content={getUpcSearch.response?.ingredients} />
+                {/* {
+                    // console.log("food: " + getUpcSearch.response?.ingredients[2].ingredient)
+                    listIngredients(getUpcSearch.response?.ingredients)
+                } */}
                 <View style={{alignSelf: 'center', paddingTop: StyleConstants.Radius}}>
                     <Text style={[Styles.labelText, {color: Colors.Foreground, marginLeft: 0}]}>{disclaimer}</Text>
                 </View>
