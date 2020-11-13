4bcb1a9855812019bdc529146fe130b4896e6622
catalinasandoval <asandoval4@wisc.edu>
2020-11-12 20:45:18 -0600

    Harmful ingredients in a food are displayed in a different color

--- src/components/Ingredients.js
+++ src/components/Ingredients.js
@@ -1,26 +1,20 @@
 import React from 'react'
-import { View, Text, StyleSheet, Pressable, Image} from 'react-native';
-import { FlatList } from 'react-native-gesture-handler'
-import { color } from 'react-native-reanimated';
-import { Styles } from '../style';
+import { View } from 'react-native';
+import { ListItem } from 'react-native-elements'
+import { Styles, Colors } from '../style';
 
 export default function Ingredients(props) {
     return (
-        <View style = {[{marginTop:50} ]}>
-            <Text style={[{ color: "white" }, {fontSize: 20}, {textAlign: 'center'}]}>
-                Ingredients:
-            </Text>
-            <FlatList
-                data = {props.content}
-                renderItem={({item}) => (
-                    <View>
-                        <Text style={[Styles.ingredientList]}> 
-                            {item.name}
-                        </Text>
-                    </View>
-                )}
-                keyExtractor={item => item}
-            />
+        <View>
+        {
+            (props.content).map((item, i) => (
+            <ListItem key={i} containerStyle={{backgroundColor: Colors.Background, padding: 2}}>
+                <ListItem.Content>
+                    <ListItem.Title style={item.safe ? [Styles.ingredientList] : [Styles.ingredientList, {color: Colors.Error}]}>{item.ingredient}</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+            ))
+        }
         </View>
     )
 }
