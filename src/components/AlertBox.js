4d9ab62f58e91fcaa523660d1cd3b7a7815271dd
Quinn Stephan <qstephan@wisc.edu>
2020-12-06 18:00:06 -0600

    Animated alert box

--- src/components/AlertBox.js
+++ src/components/AlertBox.js
@@ -1,15 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, Pressable, StyleSheet, ActivityIndicator, TextInput } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { View, Text, Pressable, StyleSheet, ActivityIndicator, TextInput, Animated } from 'react-native';
 import { Colors, StyleConstants, Styles } from '../style';
 import { MaterialIcons } from '@expo/vector-icons';
 
 export default function AlertBox({text, icon, colors}) {
-    if(!text) return null;
+    const animation = useRef(new Animated.Value(0)).current;
+
+    useEffect(() => {
+        if(text) {
+            console.log('opening');
+            Animated.timing(
+                animation,
+                {
+                    toValue: 1,
+                    duration: 300,
+                    useNativeDriver: true
+                }
+            ).start();
+        } else {
+            console.log('closing');
+            Animated.timing(
+                animation,
+                {
+                    toValue: 0,
+                    duration: 300,
+                    useNativeDriver: true
+                }
+            ).start();
+        }
+    }, [text]);
 
     return (
-        <View style={[Styles.alertBoxContainer, { borderColor: colors[5], backgroundColor: colors[1] }]}>
+        <Animated.View style={[
+            Styles.alertBoxContainer,
+            {
+                borderColor: colors[5],
+                backgroundColor: colors[1],
+                transform: [
+                    {
+                        translateY: animation.interpolate({
+                            inputRange: [0,1],
+                            outputRange: [-100, 0]
+                        })
+                    }
+                ]
+            }]}>
+
             <MaterialIcons name="error" size={32} color={colors[5]}/>
             <Text style={[Styles.alertBoxText, { color: colors[5] }]}>{text}</Text>
-        </View>
+        </Animated.View>
     )
 }
